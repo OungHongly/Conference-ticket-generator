cf886a45bc117e62ca27d649a741f8b5ae2520df
Hongly <honglyoung.123@gmail.com>
2025-09-19 22:43:37 +0700

    drag and drop image into input file

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
 "Use strict";
+
+// Select elements
+const dropArea = document.getElementById("drop-area");
+const inputFile = document.getElementById("input-file");
+const imageView = document.getElementById("image-view");
+const inputText = document.querySelector(".file__upload-text");
+const btnImage = document.querySelector(".btn__file-image");
+
+const uploadImage = function () {
+  let imgLink = URL.createObjectURL(inputFile.files[0]);
+  imageView.style.backgroundImage = `url(${imgLink})`;
+  imageView.textContent = "";
+  inputText.classList.add("hidden");
+  btnImage.classList.remove("hidden");
+};
+
+inputFile.addEventListener("change", uploadImage);
+
+dropArea.addEventListener("dragover", function (e) {
+  e.preventDefault();
+});
+
+dropArea.addEventListener("drop", function (e) {
+  e.preventDefault();
+  inputFile.files = e.dataTransfer.files;
+  uploadImage();
+});
