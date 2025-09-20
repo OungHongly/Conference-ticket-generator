641aaadbab249e60f4f6ad4a28779cbd2e3becea
Hongly <honglyoung.123@gmail.com>
2025-09-20 11:56:38 +0700

    check upload file size

--- js/script.js
+++ js/script.js
@@ -6,17 +6,52 @@ const inputFile = document.getElementById("input-file");
 const imageView = document.getElementById("image-view");
 const inputText = document.querySelector(".file__upload-text");
 const btnImage = document.querySelector(".btn__file-image");
+const uploadIcon = document.getElementById("upload--icon");
 
-const uploadImage = function () {
-  let imgLink = URL.createObjectURL(inputFile.files[0]);
-  imageView.style.backgroundImage = `url(${imgLink})`;
-  imageView.textContent = "";
+const messageHint = document.getElementById("message--hint");
+const messageError = document.getElementById("message--error");
+
+// Hide error image upload success
+const uploadSuccess = function () {
+  uploadIcon.classList.add("hidden");
+
+  messageError.classList.add("hidden");
+  messageHint.classList.remove("hidden");
+
   inputText.classList.add("hidden");
   btnImage.classList.remove("hidden");
 };
 
+//Show Error image upload
+const uploadError = function () {
+  uploadIcon.classList.remove("hidden");
+
+  messageError.classList.remove("hidden");
+  messageHint.classList.add("hidden");
+
+  inputText.classList.remove("hidden");
+  btnImage.classList.add("hidden");
+};
+
+//Upload Image function
+const uploadImage = function () {
+  const file = inputFile.files[0];
+  const maxSize = 500 * 1024;
+
+  if (file.size > maxSize) {
+    imageView.style.backgroundImage = "";
+    uploadError();
+  } else {
+    let imgLink = URL.createObjectURL(inputFile.files[0]);
+    imageView.style.backgroundImage = `url(${imgLink})`;
+    uploadSuccess();
+  }
+};
+
+// Upload image
 inputFile.addEventListener("change", uploadImage);
 
+// Drag and Drop feature
 dropArea.addEventListener("dragover", function (e) {
   e.preventDefault();
 });
